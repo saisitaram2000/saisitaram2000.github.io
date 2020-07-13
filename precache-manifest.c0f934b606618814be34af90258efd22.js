708a54ba985557b60e960933abce6a7697515b56
saisitaram2000 <saisitaramvaddavalli@gmail.com>
2020-07-13 20:17:36 +0530

    Updates

---
+++ precache-manifest.c0f934b606618814be34af90258efd22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bf055130b6059cee1cc34f940eb3ea",
+    "url": "/saisitaram2000.github.io/index.html"
+  },
+  {
+    "revision": "5fbe2537e0a88f604cf4",
+    "url": "/saisitaram2000.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "978b14dbf10ad1fd1f3f",
+    "url": "/saisitaram2000.github.io/static/js/2.04f5a9a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saisitaram2000.github.io/static/js/2.04f5a9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fbe2537e0a88f604cf4",
+    "url": "/saisitaram2000.github.io/static/js/main.c9db707d.chunk.js"
+  },
+  {
+    "revision": "2b5fb2f36325431f7b16",
+    "url": "/saisitaram2000.github.io/static/js/runtime-main.5b0429f8.js"
+  },
+  {
+    "revision": "fb2f0bcec2e26f13f8c82cbf17c00b05",
+    "url": "/saisitaram2000.github.io/static/media/profilepic.fb2f0bce.JPG"
+  }
+]);
