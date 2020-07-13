63043d39f9258dcc5ccdc5bed94ec38ac9b640c2
saisitaram2000 <saisitaramvaddavalli@gmail.com>
2020-07-13 19:58:20 +0530

    created header and about files

---
+++ myportfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ myportfolio/src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import main from '../data/main.json';
+import social from '../data/social.json';
+export default class Header extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+      
+    }
+    render(){
+
+        var networks=social.map(function(network){
+            return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        })
+        return(
+           
+            <header id="home">
+
+            <nav id="nav-wrap">
+
+                 <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+                <ul id="nav" className="nav">
+                    <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                     <li><a className="smoothscroll" href="#about">About</a></li>
+                     <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                    <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
+                    <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                </ul>
+
+            </nav>
+                <div className="row banner">
+                    <div className="banner-text">
+                        <h1 className="responsive-headline">I'm {main[0].name}.</h1>
+                        <h3>A passionate WebDeveloper and ProblemSolver</h3>
+                        <hr />
+                        <ul className="social">
+                            {networks}
+                        </ul>
+                    </div>
+                </div>
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+                </p>
+            </header>
+            
+        )
+    }
+}
